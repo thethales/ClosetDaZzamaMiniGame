3a0aa2ab03e5427e2e02c2bcd19388de144b1992
the.thales <thvms.thales@gmail.com>
2021-01-04 13:54:05 -0300

    Upload inicial das alterações do projeto exemplo

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 // the snake is divided into small segments, which are drawn and edited on each 'draw' call
 let numSegments = 10;
 let direction = 'right';
@@ -12,15 +11,154 @@ let yCor = [];
 
 let xFruit = 0;
 let yFruit = 0;
+let wFruit = 30;
+let hFruit = 30;
+
 let scoreElem;
+let controlElem;
+let font_regular;
+
+let colors = {
+  'tiffany_blue' : '#A0E7E5',
+  'hot_pink' : '#FFAEBC',
+  'yellow':'#FBE7C6',
+  'mint':'#B4F8C8',
+  'white':'#FFFFFF'
+}
+
+let canvas = {
+  'w': 500,
+  'h': 500
+}
+
+let fruit_basket = [];
+
+
+function reset() {
+  numSegments = 10;
+  direction = 'right';
+
+
+  xCor = [];
+  yCor = [];
+
+  xFruit = 0;
+  yFruit = 0;
+  wFruit = 30;
+  hFruit = 30;
+
+  setup()
+}
+ 
+function preload() {
+
+  button_press = loadSound('assets/audio/add.mp3');
+  fruit_hit = loadSound('assets/audio/mixkit-game-ball-tap-2073.wav');
+  loses_game = loadSound('assets/audio/mixkit-negative-guitar-tone-2324.wav');
+  
+
+  font_regular = loadFont('assets/fonts/Staatliches-Regular.ttf');
+
+  fruit_img = loadImage('assets/img/download.png');
+  
+  
+
+  fruit = {
+    icon: loadImage('assets/img/fruits/1.png'),
+    w: 30,
+    h: 30,
+    x: xFruit-(this.w/2),
+    y: yFruit-(this.w/2),
+    fruit_basket : [],
+    cycleIcon: function(){
+      //Lazy load of a random fruit resource
+      let seed = getRandomInt(11);
+      console.log(this.fruit_basket)
+      this.fruit_basket.push(seed);
+      console.log(this.fruit_basket)
+      src = 'assets/img/fruits/'+ seed.toString() + '.png';
+      this.icon = loadImage(src);
+    },
+    hit: function(){
+      //fruit_basket.push(this);
+      printFruitBasket(this.fruit_basket);
+      fruit_hit.play();
+    },
+    
+  }
+  
+  btn_up = {
+    'src' : 'assets/img/btn_up.png',
+    'x': 20 + this.w ,
+    'y': canvas['h'] +60,
+    'w': 80,
+    'h': 80,
+    'alt': 'Para cima',
+    click : function(){
+      controls(87);
+      button_press.play();
+    }
+  }
+
+  btn_down = {
+    'src' : 'assets/img/btn_down.png',
+    'x': 20 + this.w ,
+    'y': canvas['h'] + 60,
+    'w': 80,
+    'h': 80,
+    'alt': 'Para baixo',
+    click : function(){
+      controls(83);
+      button_press.play();
+    }
+  }
+
+  btn_left = {
+    'src' : 'assets/img/btn_left.png',
+    'x': 20 + this.w ,
+    'y': canvas['h'] +60,
+    'w': 80,
+    'h': 80,
+    'alt': 'Para a esquerda',
+    click : function(){
+      controls(65);
+      button_press.play();
+    }
+  }
+
+  btn_right = {
+    'src' : 'assets/img/btn_right.png',
+    'x': 20 + this.w ,
+    'y': canvas['h'] +60,
+    'w': 80,
+    'h': 80,
+    'alt': 'Para a direita',
+    click : function(){
+      controls(68);
+      button_press.play();
+    }
+  }
+
+}
 
 function setup() {
-  scoreElem = createDiv('Score = 0');
+  textFont(font_regular);
+  scoreElem = createDiv('Pontuação = 0');
   scoreElem.position(20, 20);
   scoreElem.id = 'score';
-  scoreElem.style('color', 'white');
+  scoreElem.style('color', colors.white);
 
-  createCanvas(500, 500);
+
+  pointsElem = createDiv();
+  pointsElem.position(501,501);
+  pointsElem.id = 'points';
+  
+
+  controlElem = createDiv();
+  controlElem.position(600,600);
+  controlElem.id = 'control';
+
+  createCanvas(windowWidth, 500);
   frameRate(15);
   stroke(255);
   strokeWeight(10);
@@ -30,10 +168,16 @@ function setup() {
     xCor.push(xStart + i * diff);
     yCor.push(yStart);
   }
+
+  //Interface
+  console.log(fruit_img)
+  image(fruit_img, 600, 650, 30,30);
+  createControlButton(btn_up, btn_down, btn_left, btn_right);
 }
 
+
 function draw() {
-  background(0);
+  background(colors['hot_pink']);
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
   }
@@ -92,8 +236,11 @@ function checkGameStatus() {
     checkSnakeCollision()
   ) {
     noLoop();
-    const scoreVal = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Game ended! Your score was : ' + scoreVal);
+    const scoreVal = getScore();//parseInt(scoreElem.html().substring(8));
+    textFont(font_regular);
+    loses_game.play();
+    scoreElem.html('O Jogo Acabou. Sua pontuação: ' + scoreVal);
+    printGameStatus()
   }
 }
 
@@ -117,15 +264,37 @@ function checkSnakeCollision() {
  I add the last segment again at the tail, thereby extending the tail)
 */
 function checkForFruit() {
-  point(xFruit, yFruit);
-  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
-    const prevScore = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Score = ' + (prevScore + 1));
+  //point(xFruit, yFruit);
+  
+  //image(fruit.icon, fruit.x, fruit.y, fruit.w, fruit.h);
+  image(fruit.icon, xFruit-(wFruit/2), yFruit-(hFruit/2), wFruit, hFruit);
+  //if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
+  if(snakeAteTheFruit()){
+    var prevScore = getScore();//parseInt(scoreElem.html().split('=')[1].trim());
+    scoreElem.html('Pontuação = ' + (prevScore + 1));
     xCor.unshift(xCor[0]);
     yCor.unshift(yCor[0]);
     numSegments++;
     updateFruitCoordinates();
+    fruit.hit();
+    fruit.cycleIcon();
   }
+}
+
+function snakeAteTheFruit(){
+  /**
+   * Artificial increase of the original fruit hitbox
+   */
+  if((xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit)){
+    return true;  
+  }
+  if((xCor[xCor.length - 1] === xFruit+10 && yCor[yCor.length - 1] === yFruit+10 )){
+    return true;  
+  }
+  if((xCor[xCor.length - 1] === xFruit-10 && yCor[yCor.length - 1] === yFruit-10 )){
+    return true;  
+  }
+  return false;
 }
 
 function updateFruitCoordinates() {
@@ -140,26 +309,117 @@ function updateFruitCoordinates() {
 }
 
 function keyPressed() {
+  controls(keyCode)
+}
+
+function controls(keyCode){
   switch (keyCode) {
-    case 74:
+    case 65:
       if (direction !== 'right') {
         direction = 'left';
       }
       break;
-    case 76:
+    case 68:
       if (direction !== 'left') {
         direction = 'right';
       }
       break;
-    case 73:
+    case 87:
       if (direction !== 'down') {
         direction = 'up';
       }
       break;
-    case 75:
+    case 83:
       if (direction !== 'up') {
         direction = 'down';
       }
       break;
   }
+
+}
+
+
+function createControlButton(){
+  /*
+    Dinamically creates a N buttons 
+    @param N arguments
+  */
+  if(arguments.length != 0){
+    let spacing = 10;
+    let x = 30;
+
+    for (var i = 0; i < arguments.length; i++){
+      btn = arguments[i];
+  
+      button = createImg(btn.src,btn.alt);
+      button.parent = controlElem;
+      button.style('width', btn.w + 'px');
+      button.style('height', btn.h + 'px');
+      button.attribute('class','btn');
+      button.position(x, btn.y);
+      button.mousePressed(btn.click);
+      x += btn.w + spacing;
+    }
+  }
+}
+
+
+
+function getScore(){
+  /*
+    Temporary improvment upon the authors original method
+    Retrieves the score value from the inner html of the upper score text on screen
+  */
+  return parseInt(scoreElem.html().split('=')[1].trim());
+}
+
+function getRandomInt(max) {
+  /**
+   * Returns a random INT number up to the max parameter value
+   */
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function printFruitBasket(arr_fruit){
+
+  
+  let max_n_icons = parseInt((windowWidth / 40)) - 1;
+  let icon_size = 40;
+
+  
+  arr_fruit = arr_fruit.slice( arr_fruit.length - max_n_icons, arr_fruit.length);
+  let a = document.getElementById('s');
+  a.innerHTML = '';
+    
+  for(var i = arr_fruit.length ; i >= 0 ; i-- ){
+    let one_fruit = arr_fruit[i];
+    if (one_fruit !== undefined){
+      button = createImg('assets/img/fruits/' + one_fruit + '.png','+1');
+      button.attribute('name','icones_ponto');
+      button.style('width', icon_size);
+      button.style('height', icon_size);
+      button.parent(document.getElementById('s'));
+    }
+    
+  }
+  
+}
+
+
+
+function printGameStatus(){
+
+  let box_x = 250;
+  let box_y = 250;
+  let box_w = 250;
+  let box_h = 100;
+  let box_r = 15;
+
+  let box_relative_x = (windowWidth/2) - (box_w/2)
+
+ 
+  rect(box_relative_x, box_y, box_w, box_h, box_r,box_r,box_r,box_r);
+
+  text('O jogo acabou', box_relative_x, box_y, box_x, box_y);
+
 }
